4180b2779d5eee24503166d7e7d5dee781286e6c
Daudi Mwangi <daudimwangi345@gmail.com>
2025-08-02 21:56:42 -0400

    removed cpp files and turned them into .h files

--- Loss.cpp
+++
@@ -1,7 +0,0 @@
-using namespace std;
-#include <vector>
-
-double mse_loss(const vector<double>& y_true, const vector<double>& y_pred);
-
-vector<double> mse_loss_derivative(const vector<double>& y_true, const vector<double>& y_pred);
-
