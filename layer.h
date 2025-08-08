d394802868c5e906a9f191f48700f91e1dbc1bb2
Daudi Mwangi <daudimwangi345@gmail.com>
2025-08-07 21:15:34 -0400

    removed the std:: prefix

--- layer.h
+++ layer.h
@@ -1,17 +1,21 @@
 using namespace std;
 #include <vector>
+#include "neuron.h"
+#include "activation.h"
 
 class Layer{
 public: 
-    Layer(int input_size, int output_size, ActivationType act_type_);
+    Layer(int input_size, int output_size, int nueron_count, ActivationType act_type_);
     vector<double> forward(const std::vector<double>& input);
     vector<double> backward(const std::vector<double>& dvalues, double learning_rate);
-    vector<*Neuron> get_neurons();
+    vector<Neuron*> get_neurons();
 
 private:
     vector<std::vector<double>> weights;
     vector<double> biases;
     ActivationType activation;
     vector<double> last_input, last_output;
+    vector<Neuron> neurons;
     int neuron_count;
+
 };
