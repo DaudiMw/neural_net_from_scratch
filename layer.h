5428459694cce91a9e248612985020d6bf5cf357
Daudi Mwangi <daudimwangi345@gmail.com>
2025-08-07 21:15:50 -0400

    simple change

--- layer.h
+++ layer.h
@@ -6,12 +6,12 @@ using namespace std;
 class Layer{
 public: 
     Layer(int input_size, int output_size, int nueron_count, ActivationType act_type_);
-    vector<double> forward(const std::vector<double>& input);
-    vector<double> backward(const std::vector<double>& dvalues, double learning_rate);
-    vector<Neuron*> get_neurons();
+    vector<double> forward(const vector<double>& input);
+    vector<double> backward(const vector<double>& dvalues, double learning_rate);
+    vector<Neuron> get_neurons();
 
 private:
-    vector<std::vector<double>> weights;
+    vector<vector<double>> weights;
     vector<double> biases;
     ActivationType activation;
     vector<double> last_input, last_output;
